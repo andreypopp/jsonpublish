ee51b705def1f7b10b8270f3ff7d35ac5e2ada09
Andrey Popp <8mayday@gmail.com>
2012-01-19 02:34:10 +0400

    github ribbon

--- docs/conf.py
+++ docs/conf.py
@@ -97,7 +97,10 @@ html_theme = 'bw'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    "github_ribbon": True,
+    "github_ribbon_link": "https://github.com/andreypopp/jsonpublish",
+    }
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = [pkg_resources.resource_filename('bw_sphinxtheme', 'themes')]
