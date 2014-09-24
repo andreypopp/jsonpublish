70424f6bc7e7b44c9b2dddeee053a5ab505cda3a
Andrey Popp <8mayday@gmail.com>
2014-09-24 15:03:52 +0400

    Pin deps

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ setup(
     packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
     test_suite="jsonpublish.tests",
     install_requires=[
-        "zope.proxy >= 3.6.1",
-        "zope.interface >= 3.8.0",
-        "repoze.lru >= 0.4",
+        "zope.proxy >= 4.1.4, < 5.0.0",
+        "zope.interface >= 4.1.1, < 5.0.0",
+        "repoze.lru >= 0.6, < 0.7",
     ],
     zip_safe=False)
