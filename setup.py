2ccd4f8b4a90bbac8d8ad63e06ca200ef76d8f6a
Andrey Popp <8mayday@gmail.com>
2014-12-03 19:04:01 +0800

    0.2.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name="jsonpublish",
-    version="0.2.0",
+    version="0.2.1",
     description="Publish Python object as JSON documents",
     long_description=open("README").read() + "\n\n" + open("CHANGES").read(),
     author="Andrey Popp",
